fb3c2cbfaecf3ab090abd5a87637d37eb177a610
Miguel Fialho <fialhomig@gmail.com>
2022-03-11 10:33:33 -0300

    Transformando blocos maiores em funções menores

--- js/form.js
+++ js/form.js
@@ -7,26 +7,9 @@
     var paciente = obtemPacienteDoFormulario(form);
     console.log(paciente);
 
-    var pacienteTr = document.createElement("tr");
+    var pacienteTr = montaTr(paciente);
     
-    var nomeTd = document.createElement("td");
-    var pesoTd = document.createElement("td");
-    var alturaTd = document.createElement("td");
-    var gorduraTd = document.createElement("td");
-    var imcTd = document.createElement("td");
-
-    nomeTd.textContent = nome;
-    pesoTd.textContent= peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
-    imcTd.textContent = calculaImc(peso,altura);
     
-
-    pacienteTr.appendChild(nomeTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-    pacienteTr.appendChild(imcTd);
 
     var tabela = document.querySelector("#tabela-pacientes");
 
@@ -46,4 +29,30 @@ function obtemPacienteDoFormulario(form){
     return paciente;
 }
 
-console.log(paciente);
+function montaTr(paciente) {
+    var pacienteTr = document.createElement("tr");
+    
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = paciente.nome;
+    pesoTd.textContent= paciente.peso;
+    alturaTd.textContent = paciente.altura;
+    gorduraTd.textContent = paciente.gordura;
+    imcTd.textContent =paciente.imc;
+    
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
+    
+    
+    
+    return pacienteTr;
+
+}
